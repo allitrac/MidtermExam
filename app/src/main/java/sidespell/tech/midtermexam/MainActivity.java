582e5eeae2e04a7f13bfaa18b632375e073fef92
nuTShELLS <claritacartilla@gmail.com>
2016-02-02 22:00:24 +0800

    Incomplete Mid

--- app/src/main/java/sidespell/tech/midtermexam/MainActivity.java
+++ app/src/main/java/sidespell/tech/midtermexam/MainActivity.java
@@ -3,8 +3,16 @@ package sidespell.tech.midtermexam;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        spinner = (ProgressBar)findViewById(R.id.progBar);
+        int i=1;
+        if(i==1){
+            spinner.setVisibility(View.VISIBLE);
+        }
+        else{
+            spinner.setVisibility(View.GONE);
+        }
+
+    /*    editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    return true;
+                }
+                    return false;
+            }
+        });*/
     }
 }
